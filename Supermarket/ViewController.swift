c779c6d71fd0000af7bca41a6aa9c982d4cbd1cd
IsaLegal <isabella@somoslegal.com>
2020-04-04 13:07:34 -0500

    Cambios

--- Supermarket/ViewController.swift
+++ Supermarket/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let manager = CoreDataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
