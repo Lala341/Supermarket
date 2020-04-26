2746aa40c5647e5e6eba20563e9e5a7061219d82
lcgaravito <lc.garavito@uniandes.edu.co>
2020-04-26 01:36:06 -0500

    Arreglos Login

--- Supermarket/Login/LoginViewController.swift
+++ Supermarket/Login/LoginViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 import Network
 
 class LoginViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var warningLoginLabel: UILabel!
+    
     var counter = 0;
     var manager = CoreDataManager();
     var usermanager = UserCoreDataManager();
@@ -43,44 +49,6 @@ class LoginViewController: UIViewController {
         }catch{
             print("Error — \(error)")
         }
-        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1")!
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("error: \(error)")
-            } else {
-                if let response = response as? HTTPURLResponse {
-                    print("statusCode: \(response.statusCode)")
-                }
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    print("data: \(dataString)")
-                }
-            }
-        }
-        task.resume()
-        
-        let url2 = URL(string: "https://jsonplaceholder.typicode.com/posts")!
-        var request = URLRequest(url: url2)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("Powered by Swift!", forHTTPHeaderField: "X-Powered-By")
-        let parameters: [String: Any] = [
-            "username": "Mundo Cruel!!!"
-        ]
-        let parametersJson = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
-        request.httpBody = parametersJson
-        let task2 = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let error = error {
-                print("error: \(error)")
-            } else {
-                if let response = response as? HTTPURLResponse {
-                    print("statusCode: \(response.statusCode)")
-                }
-                if let data = data, let dataString = String(data: data, encoding: .utf8) {
-                    print("data: \(dataString)")
-                }
-            }
-        }
-        task2.resume()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
@@ -100,7 +68,42 @@ class LoginViewController: UIViewController {
         
     }
     
-    func nextView(){
+    //MARK: Actions
+    @IBAction func loginActionButton(_ sender: UIButton) {
+        let url = URL(string: "http://ec2-18-212-16-222.compute-1.amazonaws.com:8083/users/login_user")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Powered by Swift!", forHTTPHeaderField: "X-Powered-By")
+        let parameters: [String: Any] = [
+            "email": "luis@email.com",
+            "pass_hash": "edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5"
+        ]
+        let parametersJson = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+        request.httpBody = parametersJson
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print("error: \(error)")
+            } else {
+                if let response = response as? HTTPURLResponse, let data = data, let dataString = String(data: data, encoding: .utf8) {
+                    if response.statusCode == 403 {
+                        DispatchQueue.main.async {
+                            self.warningLoginLabel.text = dataString
+                        }
+                    }
+                    else if response.statusCode == 200 {
+                        //TODO: Save the user_id from dataString
+                        DispatchQueue.main.async {
+                            self.goTabBarView()
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func goTabBarView(){
         
         let VC = self.storyboard!.instantiateViewController(withIdentifier: "TabBarView") as! TabBarViewController
         VC.modalPresentationStyle = .fullScreen
