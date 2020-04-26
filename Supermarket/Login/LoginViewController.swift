2afded505ea7f0735b7219459b7ff7c66a17b1a3
lcgaravito <lc.garavito@uniandes.edu.co>
2020-04-26 04:08:11 -0500

    Login terminado

--- Supermarket/Login/LoginViewController.swift
+++ Supermarket/Login/LoginViewController.swift
@@ -9,8 +9,9 @@
 import Foundation
 import UIKit
 import Network
+import CryptoKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     @IBOutlet weak var emailTextField: UITextField!
@@ -24,6 +25,8 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
         do {
             networkMonitor.pathUpdateHandler = { path in
                 
@@ -67,17 +70,27 @@ class LoginViewController: UIViewController {
         
         
     }
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
     
     //MARK: Actions
     @IBAction func loginActionButton(_ sender: UIButton) {
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let passwordSHA246 = sha256(password)
+        
         let url = URL(string: "http://ec2-18-212-16-222.compute-1.amazonaws.com:8083/users/login_user")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("Powered by Swift!", forHTTPHeaderField: "X-Powered-By")
         let parameters: [String: Any] = [
-            "email": "luis@email.com",
-            "pass_hash": "edf9cf90718610ee7de53c0dcc250739239044de9ba115bb0ca6026c3e4958a5"
+            "email": email,
+            "pass_hash": passwordSHA246
         ]
         let parametersJson = try? JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
         request.httpBody = parametersJson
@@ -103,12 +116,17 @@ class LoginViewController: UIViewController {
         task.resume()
     }
     
+    func sha256(_ data: String) -> String{
+        let dataData = Data(data.utf8)
+        let hashed = SHA256.hash(data: dataData)
+        let hashString = hashed.compactMap { String(format: "%02x", $0) }.joined()
+        return hashString
+    }
+    
     func goTabBarView(){
-        
         let VC = self.storyboard!.instantiateViewController(withIdentifier: "TabBarView") as! TabBarViewController
         VC.modalPresentationStyle = .fullScreen
         self.present(VC, animated: true, completion: nil)
         self.show(VC, sender: self)
-        
     }
 }
